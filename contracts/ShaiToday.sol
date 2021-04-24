c86d7a28e82cdcfad162bf895efa7f9fa850b142
Max Tyrrell <maxtyrrell0@gmail.com>
2021-04-24 16:19:45 +0200

    feat: implement ERC721Enumerable

--- contracts/ShaiToday.sol
+++ contracts/ShaiToday.sol
@@ -2,14 +2,32 @@
 pragma solidity ^0.7.3;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ShaiToday is ERC721, Ownable {
+contract ShaiToday is ERC721, ERC721Enumerable, Ownable {
     constructor() ERC721("ShaiToday", "SHAI") {
-      _setBaseURI("https://shai.today/api/token/");
+        _setBaseURI("https://shai.today/api/token/");
     }
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
