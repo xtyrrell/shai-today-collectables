43dd7f9f89fd0d082b47dec15e71bf15efa7ceba
Max Tyrrell <maxtyrrell0@gmail.com>
2021-04-24 15:29:49 +0200

    feat: implement simple ShaiToday ERC721 contract

---
+++ contracts/ShaiToday.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.3;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ShaiToday is ERC721, Ownable {
+    constructor() ERC721("ShaiToday", "SHAI") {
+      _setBaseURI("https://shai.today/api/token/");
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
